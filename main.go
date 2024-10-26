498ade07f2e6db971c8305096aedbd37eab43352
nhutran <tknhu130@gmail.com>
2024-10-27 05:35:23 +0700

    update cors in golang

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	"go-tutorial/utils"
 	"log"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -20,6 +22,14 @@ func main() {
 	gin.SetMode(configs.AppConfig.Server.Mode)
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://trankhacnhu.com"}, // Allow specific origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},                     // Allow specific HTTP methods
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},          // Allow specific headers
+		ExposeHeaders:    []string{"Content-Length"},                                   // Expose specific headers
+		AllowCredentials: true,                                                         // Allow credentials (cookies, etc.)
+		MaxAge:           12 * time.Hour,                                               // Preflight request cache duration
+	}))
 	router.Use(utils.Logger())
 
 	db := internal.InitDB()
